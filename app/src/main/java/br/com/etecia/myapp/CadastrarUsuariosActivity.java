d8665e0294dbf90c3ba580271d284f6ca13f1eb8
lndsilva <lndsilva@hotmail.com>
2025-10-30 20:18:27 -0300

    janela de cadastrar

--- app/src/main/java/br/com/etecia/myapp/CadastrarUsuariosActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CadastrarUsuariosActivity.java
@@ -1,14 +1,25 @@
 package br.com.etecia.myapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
+import java.security.PrivateKey;
+
 public class CadastrarUsuariosActivity extends AppCompatActivity {
+    MaterialButton btnVoltar, btnCadastrar;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,44 @@ public class CadastrarUsuariosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(CadastrarUsuariosActivity.this);
+                builder.setTitle("Sistema de cadastro");
+                builder.setMessage("Cadastrado com sucesso!!!");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Cadastrado com sucesso", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
     }
+
 }
