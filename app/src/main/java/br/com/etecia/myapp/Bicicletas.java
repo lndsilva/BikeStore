29eebfe96aa069c0683577aeaa6cde8bbe9704f8
lndsilva <lndsilva@hotmail.com>
2025-11-27 20:21:20 -0300

    trabalhando com listas

---
+++ app/src/main/java/br/com/etecia/myapp/Bicicletas.java
@@ -0,0 +1,29 @@
+package br.com.etecia.myapp;
+
+public class Bicicletas {
+    //variáveis globais
+    private String titulo;
+    private int imgBike;
+
+    //criando construtor vazio
+    public Bicicletas() {
+    }
+    //criando o getter and setter
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImgBike() {
+        return imgBike;
+    }
+
+    public void setImgBike(int imgBike) {
+        this.imgBike = imgBike;
+    }
+}
