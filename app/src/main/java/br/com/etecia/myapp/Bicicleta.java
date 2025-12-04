c4627c1c0443f1b7b9e071e7e5a56a48ddf840b1
lndsilva <lndsilva@hotmail.com>
2025-12-04 19:33:58 -0300

    adaptador

---
+++ app/src/main/java/br/com/etecia/myapp/Bicicleta.java
@@ -0,0 +1,29 @@
+package br.com.etecia.myapp;
+
+public class Bicicleta {
+    private String titulo;
+    private int imgBicicleta;
+
+    //alt+insert
+    //construtor
+    public Bicicleta() {
+    }
+    //getters and setters
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImgBicicleta() {
+        return imgBicicleta;
+    }
+
+    public void setImgBicicleta(int imgBicicleta) {
+        this.imgBicicleta = imgBicicleta;
+    }
+}
