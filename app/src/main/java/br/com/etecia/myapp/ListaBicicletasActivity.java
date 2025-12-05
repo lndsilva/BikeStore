7e38e0426e441a2e2a890fe4a368f6d652937784
lndsilva <lndsilva@hotmail.com>
2025-12-04 21:17:41 -0300

    adaptador

--- app/src/main/java/br/com/etecia/myapp/ListaBicicletasActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaBicicletasActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class ListaBicicletasActivity extends AppCompatActivity {
+    List<Bicicleta> lstBicicleta;
+    RecyclerView listaBikes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,8 @@ public class ListaBicicletasActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //RecyclerView
+        listaBikes = findViewById(R.id.listaBikes);
+
     }
 }
