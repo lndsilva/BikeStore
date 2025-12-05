420c8a0311ed01f75edeaa6d3124ca63463fe43b
lndsilva <lndsilva@hotmail.com>
2025-12-04 21:49:06 -0300

    adaptador

--- app/src/main/java/br/com/etecia/myapp/ListaBicicletasActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaBicicletasActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -29,12 +30,12 @@ public class ListaBicicletasActivity extends AppCompatActivity {
         //criando um array de dados
         lstBicicleta = new ArrayList<>();
         lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
-        lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
-        lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
-        lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
-        lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
-        lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
-        lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
+        lstBicicleta.add(new Bicicleta("Trail",R.drawable.bike_trail_icon));
+        lstBicicleta.add(new Bicicleta("Gravel",R.drawable.bike_gravel_icon));
+        lstBicicleta.add(new Bicicleta("Urbana",R.drawable.bike_urbana));
+        lstBicicleta.add(new Bicicleta("Speed",R.drawable.bike_speed_icon));
+        lstBicicleta.add(new Bicicleta("Urbana",R.drawable.bike_urbana_icon));
+        lstBicicleta.add(new Bicicleta("Estrada",R.drawable.estrada));
         lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
         lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
         lstBicicleta.add(new Bicicleta("Passeio",R.drawable.logo));
@@ -43,6 +44,10 @@ public class ListaBicicletasActivity extends AppCompatActivity {
         listaBikes = findViewById(R.id.listaBikes);
 
         BicicletaAdapter adapter = new BicicletaAdapter(lstBicicleta,ListaBicicletasActivity.this);
+
+        listaBikes.setLayoutManager(new
+                GridLayoutManager(getApplicationContext(),
+                2));
 
         //Liga o adaptador a lista
         listaBikes.setAdapter(adapter);
